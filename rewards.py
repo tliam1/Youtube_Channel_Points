d0b62604dceb2b93065d18ffeeb7754c3cf1eaaf
Liam Tapper <97718694+tliam1@users.noreply.github.com>
2024-09-29 13:10:54 -1000

    Rewards with more than chat response

--- rewards.py
+++ rewards.py
@@ -8,8 +8,32 @@ rewards = {
     "tierlist": {
         "price": 1000,
         "description": "I make a tier list. Category is up to you!"
+    },
+    "say": {
+        "price": 200,
+        "description": "TTS message based on what is written after \"say\" in the redeem msg."
     }
 }
+
+reward_callables = {
+    "say": {
+        "callable": lambda message: call_TTS(message)
+    }
+}
+
+
+def call_TTS(msg):
+    pass
+
+
+def handle_reward(reward_name, *args):
+    if is_valid_reward(reward_name):
+        if reward_name in reward_callables and 'callable' in reward_callables[reward_name]:
+            reward_callables[reward_name]['callable'](*args)
+        else:
+            print(f"Reward '{reward_name}' does not have a callable action.")
+    else:
+        print(f"Invalid reward: {reward_name}")
 
 
 def is_valid_reward(reward_name):
@@ -23,4 +47,3 @@ def rewards_to_string():
 
     # Return the JSON string as it is
     return json_string
-
